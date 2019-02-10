d6ea5a2c37dd45ef1803df476943236cca607a92
Joseph Lewkovich <jlewkovich@mozzor.com>
2019-02-10 17:14:08 -0500

    testing using CDN

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import builtins from "rollup-plugin-node-builtins";
+import globals from "rollup-plugin-node-globals";
+
+export default {
+  input: "lib/modules.js",
+  output: {
+    file: "lib/index.mjs",
+    format: "es",
+    browser: true
+  },
+  plugins: [
+    globals(),
+    builtins(),
+    resolve({
+      preferBuiltins: false,
+      browser: true,
+      customResolveOptions: {
+        moduleDirectory: "node_modules"
+      }
+    }),
+    commonjs()
+  ]
+};
